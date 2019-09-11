35bd6b71b9b90e7f118ccffedbe5dc60fbb4851f
MaiUbeid <ubead95@gmail.com>
2019-09-11 11:46:37 +0300

    code puzzle component to add board and shuffle it Relates #3

---
+++ src/Component/Board.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+// eslint-disable-next-line react/prefer-stateless-function
+export default class Board extends React.Component {
+  render() {
+    return (
+      <div>board</div>
+    );
+  }
+}
---
+++ src/Component/Cell.js
@@ -0,0 +1,4 @@
+import React from 'react';
+
+export default class Cell extends React.Component {
+}
